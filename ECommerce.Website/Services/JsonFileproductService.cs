8d0044e4306a5122ee2d40ea04cb978d9ae39145
Diána Szántó <szanto.diana.1992@gmail.com>
2021-06-22 10:41:37 +0200

    work in progress

---
+++ ECommerce.Website/Services/JsonFileproductService.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.Website.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace ECommerce.Website.Pages
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using var jsonFileReader = File.OpenText(JsonFileName);
+            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+    }
+}
